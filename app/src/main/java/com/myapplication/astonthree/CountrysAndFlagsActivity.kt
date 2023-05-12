fb80ea92a319676598170e1f5943aa2535237d40
Viktor <vincet1998@gmail.com>
2023-05-12 17:31:20 +0300

    add flags view and names

---
+++ app/src/main/java/com/myapplication/astonthree/CountrysAndFlagsActivity.kt
@@ -0,0 +1,11 @@
+package com.myapplication.astonthree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CountrysAndFlagsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_countrys_and_flags)
+    }
+}
--- app/src/main/java/com/myapplication/astonthree/MainActivity.kt
+++ app/src/main/java/com/myapplication/astonthree/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.myapplication.astonthree
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn_countries:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_countries = findViewById(R.id.button_countries)
+        btn_countries.setOnClickListener {
+            startCountriesActivity()
+        }
+    }
+
+    fun startCountriesActivity(){
+        val intent = Intent(this,CountrysAndFlagsActivity::class.java)
+        startActivity(intent)
     }
 }
